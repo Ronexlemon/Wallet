040b72852c38682ad61096c2421f30a688a493d2
RonexLemon <ronexondimu@gmail.com>
2023-03-01 15:44:29 +0300

    implement functionality to create an account

--- src/components/CreateAccout.jsx
+++ src/components/CreateAccout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 
 import   { generateMnemonic, mnemonicToEntropy } from "ethereum-cryptography/bip39";
 import  { wordlist } from "ethereum-cryptography/bip39/wordlists/english";
@@ -9,12 +9,45 @@ import  { bytesToHex } from "ethereum-cryptography/utils";
 
 
 const CreateAccount =()=>{
+    const [useraddress,setUserAddress] = useState();
+    const [seedPhrase,setMnemonic] = useState();
+
     const strength = 128;
     const _generateMnemonic =()=>{
         const mnemonic = generateMnemonic(wordlist,strength);
         const entropy = mnemonicToEntropy(mnemonic,wordlist);
         return {mnemonic,entropy}
         
+    }
+    const __getHdRootKey = (_mnemonic)=>{
+        return HDKey.fromMasterSeed(_mnemonic);
+
+    }
+    const _generatePrivateKey=(_hdRootKey,_accountIndex)=>{
+        return _hdRootKey.deriveChild(_accountIndex).privateKey;
+    }
+    const _getPublicKey =(_privateKey)=>{
+        return getPublicKey(_privateKey);
+
+    }
+    const _getEthAddress=(_publicKey)=>{
+        return keccak256(_publicKey).slice(-20);
+    }
+    const getUserAccount =(_address)=>{
+      const account =  `0x${bytesToHex(_address)}`
+      
+      return account;
+
+    }
+    const createUserAccount =async()=>{
+        const {mnemonic,entropy} = generateMnemonic();
+        setMnemonic(mnemonic);
+        const hdRootKey = __getHdRootKey(mnemonic);
+        const userPrivateKey = _generatePrivateKey(hdRootKey);
+        const userPublicKey = _getPublicKey(userPrivateKey);
+        const userAddress = _getEthAddress(userPrivateKey);
+       const account =  getUserAccount(userAddress);
+       setUserAddress(account);
     }
     return(
         <div className="w-full h-8 flex justify-center">
