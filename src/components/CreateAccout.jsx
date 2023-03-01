60365959e27241c28f05f1087c427c5929b67c7b
RonexLemon <ronexondimu@gmail.com>
2023-03-01 15:22:05 +0300

    import all lib for creating of an account

--- src/components/CreateAccout.jsx
+++ src/components/CreateAccout.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+import   { generateMnemonic, mnemonicToEntropy } from "ethereum-cryptography/bip39";
+import  { wordlist } from "ethereum-cryptography/bip39/wordlists/english";
+import  { HDKey } from "ethereum-cryptography/hdkey";
+import  { getPublicKey } from "ethereum-cryptography/secp256k1";
+import { keccak256 } from "ethereum-cryptography/keccak";
+import  { bytesToHex } from "ethereum-cryptography/utils";
+import { writeFileSync,readFileSync } from "fs";
+
 const CreateAccount =()=>{
     return(
         <div className="w-full h-8 flex justify-center">
